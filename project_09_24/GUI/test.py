e2e88e7eb04afd834ec8c9f31d551852a2792915
inteee1 <131097590+inteee1@users.noreply.github.com>
2025-09-24 16:51:34 +0900

    2025_09_24

---
+++ project_09_24/GUI/test.py
@@ -0,0 +1,167 @@
+import sys, requests, numpy as np, cv2
+from PySide6.QtWidgets import QApplication, QLCDNumber, QPushButton, QLabel, QFileDialog, QMessageBox
+from PySide6.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsPixmapItem
+from PySide6.QtCore import QFile, Qt, QTimer
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtGui import QImage, QPixmap
+
+class CrosswalkSign():
+    def __init__(self, signWidget, LCDWidget, startCount=10, blinkInterval=500):
+        self.sign = signWidget
+        self.LCD = LCDWidget
+        
+        self.greenLightOnImg = QPixmap('/home/inteee/opencvZoo2/imageproject/data/GUI_image/greenLightOn.png')
+        self.greenLightOffImg = QPixmap('/home/inteee/opencvZoo2/imageproject/data/GUI_image/greenLightOff.png')
+        self.redLightImg = QPixmap('/home/inteee/opencvZoo2/imageproject/data/GUI_image/redLight.png')
+
+        self.isGreen = True  # 신호등이 초록 불인지
+        self.greenOn = True  # 초록불 깜빡임 상태
+        self.countNum = startCount
+        self.startCount = startCount
+        self.LCDVisible = True  # LCD 깜빡임 상태
+
+        # LCD 초기값
+        #self.setLCDValue(self.countNum)
+
+        # 타이머 설정
+        self.blinkTimer = QTimer()
+        self.blinkTimer.timeout.connect(self.blink)
+        self.blinkTimer.start(blinkInterval)  # ms
+        
+
+    def setLCDValue(self, val):
+        self.LCD.display(val)
+
+    def blink(self):
+        # 신호등 초록불 깜빡이
+        if self.isGreen:
+            if self.greenOn and self.countNum < self.startCount // 2:
+                self.sign.setPixmap(self.greenLightOffImg)
+            else:
+                self.sign.setPixmap(self.greenLightOnImg)
+            self.greenOn = not self.greenOn
+        else:
+            self.sign.setPixmap(self.redLightImg)
+
+        # LCD 깜빡이 & 감소
+        if self.LCDVisible:
+            self.LCD.setStyleSheet("color: #072908;")
+            
+        else:
+            self.countNum -= 1
+            if self.countNum <= 0:
+                self.sign.setPixmap(self.redLightImg)
+                self.setLCDValue('')
+                self.blinkTimer.stop()
+                return
+            self.setLCDValue(self.countNum)
+            if self.countNum < self.startCount // 2:
+                self.LCD.setStyleSheet("color: #20b220;")
+        self.LCDVisible = not self.LCDVisible
+            
+class MainWindow():
+    def __init__(self):
+        #변수 선언
+        self.lightIsGreen = True
+        self.currentFrame = -1
+        self.MaximumFrame = 0
+        self.crosswalk = None
+        
+        #어플리케이션 객체 생성 및 ui 파일 불러오기
+        self.app = QApplication([])
+        uiFile = QFile('/home/inteee/opencvZoo2/imageproject/GUI/mainWindow.ui')
+        uiFile.open(QFile.ReadOnly)
+        loader = QUiLoader()
+        self.window = loader.load(uiFile)
+        uiFile.close()
+        
+        #GUI 위젯 생성 및 초기화
+
+        self.signLabel = self.window.findChild(QLabel, "signLabel")
+        self.countLCD = self.window.findChild(QLCDNumber, "countLCD")
+        self.countLCD.setStyleSheet("color: #20b220;")
+        self.countLCD.display('')
+
+        self.testButton = self.window.findChild(QPushButton, "testButton")
+        self.testButton.clicked.connect(self.testButtonAct)
+        
+        print(self.testButton)
+
+        self.cctvButton = self.window.findChild(QPushButton, "cctvButton")
+        self.cctvButton.clicked.connect(self.testCCTVLive)
+        
+        self.videoFileButton = self.window.findChild(QPushButton, "videoFileButton")
+        self.videoFileButton.clicked.connect(self.openVideoFile)
+        
+        self.graphicsView = self.window.findChild(QGraphicsView, "graphicsView")
+        self.graphicsScene = QGraphicsScene()
+        self.graphicsView.setScene(self.graphicsScene)
+        self.pixmapItem = QGraphicsPixmapItem()
+        self.graphicsScene.addItem(self.pixmapItem)
+        
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.nextFrame)
+            
+    def run(self):
+        self.window.show()
+        sys.exit(self.app.exec())
+    
+    def testButtonAct(self):
+        self.crosswalk = CrosswalkSign(self.signLabel, self.countLCD, startCount=8, blinkInterval=500)
+        
+    def testCCTVLive(self):
+        pass
+        
+            
+    def openVideoFile(self):
+        filePath,_ = QFileDialog.getOpenFileName(
+            self.window,
+            "비디오 파일을 선택하세요.",
+            "./",
+            "Videos (*.mp4 *.MP4 *.mkv *.MKV)"
+        )
+        if filePath:
+            self.videoCap = cv2.VideoCapture(filePath)
+            if not self.videoCap.isOpened():
+                QMessageBox.warning(self.window, "에러", "비디오를 열 수 없습니다.")
+                return
+        else:
+            return
+
+        #비디오 변수 초기화
+        #self.fps = self.videoCap.get(cv2.CAP_PROP_FPS)
+        self.currentFrame = 0
+        self.MaximumFrame = int(self.videoCap.get(cv2.CAP_PROP_FRAME_COUNT))        
+        #print(f'selected file: {filePath}')
+        #print(f"해상도: {self.videoCap.get(cv2.CAP_PROP_FRAME_WIDTH)}x{self.videoCap.get(cv2.CAP_PROP_FRAME_HEIGHT)}")
+        #print(f"FPS: {self.videoCap.get(cv2.CAP_PROP_FPS)}")
+        #print(f"총 프레임: {self.videoCap.get(cv2.CAP_PROP_FRAME_COUNT)}")
+        
+        self.nextFrame()
+        self.graphicsScene.setSceneRect(self.pixmapItem.boundingRect())
+        self.graphicsView.fitInView(self.pixmapItem, Qt.KeepAspectRatio)
+        fps = 60
+        interval = int(1000 / fps) if fps > 0 else 33
+        self.timer.start(interval)
+    
+    def nextFrame(self, ret = None, frame = None):
+        if ret is None:
+            ret, frame = self.videoCap.read()
+        self.currentFrame += 1
+        if self.currentFrame >= self.MaximumFrame:
+            self.videoCap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            self.currentFrame = -1
+        if ret:
+            self.replaceViewer(frame)
+            
+    def replaceViewer(self, frame):
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb_frame.shape
+        qimg = QImage(rgb_frame.data, w, h, w * ch, QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(qimg)
+        
+        self.pixmapItem.setPixmap(pixmap)
+    
+if __name__ == "__main__":
+    app = MainWindow()
+    app.run()
---
+++ project_09_24/detect_traffic.py
@@ -0,0 +1,93 @@
+import torch
+import numpy as np
+import cv2
+from utils.datasets import letterbox
+from utils.general import non_max_suppression, scale_coords
+from utils.plots import Annotator
+from importpath import ImportPath
+import sys
+sys.path.append(ImportPath.TRAFFIC_MODEL_PATH)
+
+
+class Detect_Traffic:
+    def __init__(self, Model_path, img_size=640, conf_thres=0.5, iou_thres=0.45):
+        self.img_size = img_size
+        self.conf_thres = conf_thres  # confidence threshold
+        self.iou_thres = iou_thres  # NMS IOU threshold
+        self.max_det = 1000  # maximum detections per image
+        self.classes = None  # filter by class
+        self.agnostic_nms = False  # class-agnostic NMS
+        self.crosswalk_points = []  # 횡단보도 좌표
+        self.traffic_status = "신호등 탐지 안됨" #  신호등 상태
+
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.ckpt = torch.load(Model_path, map_location=self.device, weights_only=False)
+        self.model = self.ckpt['ema' if self.ckpt.get('ema') else 'model'].float().fuse().eval()
+        self.class_names = ['횡단보도', '빨간불', '초록불'] # model.names
+        self.stride = int(self.model.stride.max())
+        self.colors = ((50, 50, 50), (0, 0, 255), (0, 255, 0)) # (gray, red, green)
+         
+    def preprocess_traffic(self, frame):
+        # preprocess
+        img_input = letterbox(frame, self.img_size, stride=self.stride)[0]
+        img_input = img_input.transpose((2, 0, 1))[::-1]
+        img_input = np.ascontiguousarray(img_input)
+        img_input = torch.from_numpy(img_input).to(self.device)
+        img_input = img_input.float()
+        img_input /= 255.
+        img_input = img_input.unsqueeze(0)
+        return img_input
+
+    def infer(self, frame):
+
+        # inference 횡단보도,신호등
+        img_input = self.preprocess_blinker(frame)
+        
+        pred = self.model(img_input, augment=False, visualize=False)[0]
+
+        # postprocess
+        pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, self.classes, self.agnostic_nms, max_det=self.max_det)[0]
+
+        pred = pred.cpu().numpy()
+ 
+        pred[:, :4] = scale_coords(img_input.shape[2:], pred[:, :4], frame.shape).round()
+        
+        return pred
+
+    def detect_traffic(self, frame):
+
+        self.crosswalk_points = []
+        pred = self.infer(frame)
+        
+         # Visualize
+        annotator = Annotator(frame.copy(), line_width=3, example=str(self.class_names), font=ImportPath.FONT_PATH)
+
+        #cw_x1, cw_x2 = None, None # 횡단보도 좌측(cw_x1), 우측(cw_x2) 좌표
+        self.traffic_status = "신호등 탐지 안됨" # 신호등 값 초기화
+        points = []
+        if pred is not None:
+            for p in pred:
+                cls_id = int(p[5])
+                class_name = self.class_names[cls_id]
+                x1, y1, x2, y2 = p[:4]
+                annotator.box_label([x1,y1,x2,y2], f"{class_name} {p[4]*100:.1f}%", color=self.colors[cls_id])
+                 
+                if class_name == "횡단보도":
+                    # 횡단보도 영역 좌표 계산 (좌상단, 우상단, 우하단, 좌하단)
+                    # 좌표를 4점 배열로 변환
+                    points = np.array([
+                        [x1, y1],  # 좌상단
+                        [x2, y1],  # 우상단
+                        [x2, y2],  # 우하단
+                        [x1, y2],  # 좌하단
+                    ], dtype=int)
+                    self.crosswalk_points.append(points)  # 리스트에 추가
+                
+                elif class_name in ["빨간불", "초록불"]: 
+                    self.traffic_status = class_name    # 신호등 상태 저장
+                   
+        result_img = annotator.result()
+         
+        return result_img, points, self.traffic_status
+        
+    
